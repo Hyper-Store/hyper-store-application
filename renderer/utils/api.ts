f482db707eae1e9c123083ca4ac56440acf21cd0
Daniel-D3V <contatodanielsilvaoficial@gmail.com>
2023-07-21 22:04:26 -0300

    save

--- renderer/utils/api.ts
+++ renderer/utils/api.ts
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import 'dotenv/config';
 
 export const axios = Axios.create({
-    baseURL: 'http://207.32.218.146',
+    baseURL: 'https://api.leinadhosting.app',
     validateStatus: (status) => true,
     withCredentials: true
 })
