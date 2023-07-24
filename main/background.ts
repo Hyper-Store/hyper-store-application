2994039b19c3fe33f65d0387de9a6fe633e30c46
Daniel-D3V <contatodanielsilvaoficial@gmail.com>
2023-07-24 11:02:12 -0300

    save

--- main/background.ts
+++ main/background.ts
@@ -1,4 +1,4 @@
-import { app, dialog, autoUpdater, Response } from 'electron';
+import { app, dialog, Response, autoUpdater } from 'electron';
 import 'dotenv/config';
 import serve from 'electron-serve';
 import { createWindow } from './helpers';
@@ -45,7 +45,6 @@ if (isProd) {
   } else {
     const port = process.argv[2];
     await mainWindow.loadURL(`http://localhost:${port}/home`);
-    // mainWindow.webContents.toggleDevTools()
   }
 })();
 
@@ -53,40 +52,7 @@ app.on('ready', () => {
   if (!app.isEmojiPanelSupported()) {
     dialog.showErrorBox('Houve um erro', 'Pareçe que seu computador não possui suporte para emojis, pode haver alguns problemas de fontes em seu computador, para mais informações acesse: https://discord.gg/hRzyVVWW3U')
   }
-})
-
-
-app.on('ready', () => {
-  autoUpdater.setFeedURL({ url: `https://github.com/Hyper-Store/hyper-store-applications-update/releases/download/update`, });
-  autoUpdater.checkForUpdates();
-
-  autoUpdater.on('update-available', async () => {
-    await dialog.showMessageBox({
-      type: 'info',
-      buttons: ['OK'],
-      defaultId: 0,
-      title: 'Atualização Necessária',
-      message: 'Uma nova versão do aplicativo está disponível. A atualização é obrigatória para continuar utilizando o aplicativo.',
-      detail: 'O aplicativo será reiniciado após a atualização.',
-    })
-
-    app.quit();
-  });
-
-  autoUpdater.on('error', (error) => {
-    dialog.showErrorBox('Erro na atualização', 'Houve um erro ao tentar atualizar o aplicativo, tente reinstalar ou caso não consiga entre em contato com o suporte na Hyper Store!')
-  });
-
-  autoUpdater.on('update-downloaded', () => {
-    dialog.showMessageBox({
-      type: 'info',
-      buttons: ['OK'],
-      defaultId: 0,
-      title: 'Atualização em andamento',
-      message: 'O aplicativo já está sendo atualizado, fique tranquilo assim que pronto ele vai estar disponível para abrir novamente!',
-    })
-  });
-})
+});
 
 app.on('window-all-closed', () => {
   app.quit();
